0a89990d2dd3e45620fdbde76e59fde40d4f4dc5
sandip-31 <chavdasandiph@gmail.com>
2021-08-04 15:52:53 +0530

    first commit

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const path = require('path')
 const app = express();
 
 const dbconfig = require("./db")
@@ -14,6 +14,16 @@ app.use("/api/users", usersRoute)
 app.use("/api/bookings", bookingsRoute)
 
 
+if (process.env.NODE_ENV === 'production') {
+
+    app.use('/', express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
+    })
+}
+
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log("node server started using nodemon"))
+
