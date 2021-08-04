59a5d4472e5f4cd706a90f8b8da240d01f3cdde5
sandip-31 <chavdasandiph@gmail.com>
2021-08-04 16:46:29 +0530

    first commit

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(port, () => console.log("node server started using nodemon"))
+app.listen(PORT, () => console.log("node server started using nodemon"))
 
